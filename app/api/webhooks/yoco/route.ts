164a92a6a7eabcaa1948ab47b7f90391e0cd88c8
LaudemDeiTickets <laudemdeitickets@gmail.com>
2025-10-04 01:40:40 +0200

    Delete app/api directory

--- app/api/webhooks/yoco/route.ts
+++
@@ -1,9 +0,0 @@
-export const runtime = "nodejs";
-export const dynamic = "force-dynamic";
-
-export async function POST(req: Request) {
-  const body = await req.json().catch(() => ({}));
-  const checkoutId = body?.data?.checkout?.id || body?.data?.metadata?.checkoutId || body?.data?.id;
-  // TODO: mark order with this checkoutId as 'paid' in your DB
-  return Response.json({ received: true, checkoutId });
-}
